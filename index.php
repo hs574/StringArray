d7a7147a705f048de630dcc4157b288ed598eb30
Harshit Mayur Shah <hs574@afsconnect1.njit.edu>
2017-09-19 20:36:40 -0400

    Strings commit

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $obj = new main();
 $obj ->printString($textInput);
 $obj ->stringLength($textInput);
 class main {
-public function_construct() {
-echo 'hello world';
-}
+//public function_construct() {
+//echo 'hello world';
+//}
 public function printString($text) {
 echo '<h1>print Function example</h1>';
 echo 'Print:';
@@ -15,8 +15,6 @@ echo '<hr>';
 }
 public function stringLength($text){
 echo '<h1> String Length Example</h1>';
-}
-public function_destruct() {
 }
 }
 ?>php
