93ae7678c436a706ef76d4842f381414a72a3272
Harshit Mayur Shah <hs574@afsconnect1.njit.edu>
2017-09-25 17:27:05 -0400

    String repeat function commit

--- index.php
+++ index.php
@@ -1,23 +1,44 @@
 <?php
-$textInput='Harshit Shah';
-$obj = new main();
-$obj ->printString($textInput);
-$obj ->stringLength($textInput);
-class main {
-//public function_construct() {
-//echo 'hello world';
-//}
-public function printString($text) {
-echo '<h1>print Function example</h1>';
-echo 'Print:';
-print($text);
-echo '<hr>';
-}
-public function stringLength($text){
-echo '<h1> String Length Example</h1>';
-}
-}
-?>php
+
+  $obj = new main();
+  $text = "my text";
+    $obj->printthis($text);
+
+  $array = array(1,2,3,4,5,6,7);
+  
+
+  class main {
+
+    public function __construct() {
+
+      echo '10 string functions/b> </br>';
+
+    }
+
+    public function str_repeat($text) {
+      echo '<h1>String repeat function demo </h1>';
+      echo str_repeat("$text", 10);
+      echo '<hr>';
+    }
+
+    public function printArray($array) {
+      echo '<h1>array print function</h1>';
+      print_r($array);
+      echo '<hr>';
+    }
+    
+    public function __destruct() {
+
+      echo '</br> I\'m Done';
+
+    }
+
+
+  }
+
+
+
+?>
 
 
 
